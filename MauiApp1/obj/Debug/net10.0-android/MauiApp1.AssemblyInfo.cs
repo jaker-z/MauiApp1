2232510ce24a17b5ad15a792f97304fa0068a5f1
Jake Hope <jacob.hope@icloud.com>
2026-09-22 20:45:25 -0700

    Added more files from MSI Pc

--- MauiApp1/obj/Debug/net10.0-android/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net10.0-android/MauiApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b8ab5873ee460b85a6b1f1e83ae64c66ee5824f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
